b70d072f7d77ae7bcb5e569b52320c8d75070603
chris <chris@xorrior.com>
2017-08-10 12:06:39 -0400

    small change

--- RemoteRecon.js
+++ RemoteRecon.js
@@ -1,12 +1,12 @@
 function setversion() {
-var shell = new ActiveXObject('WScript.Shell');
-ver = 'v4.0.30319';
-try {
-shell.RegRead('HKLM\\SOFTWARE\\Microsoft\\.NETFramework\\v4.0.30319\\');
-} catch(e) { 
-ver = 'v2.0.50727';
-}
-shell.Environment('Process')('COMPLUS_Version') = ver;
+	var shell = new ActiveXObject('WScript.Shell');
+	ver = 'v4.0.30319';
+	try {
+	shell.RegRead('HKLM\\SOFTWARE\\Microsoft\\.NETFramework\\v4.0.30319\\');
+	} catch(e) { 
+	ver = 'v2.0.50727';
+	}
+	shell.Environment('Process')('COMPLUS_Version') = ver;
 
 }
 function debug(s) {}
